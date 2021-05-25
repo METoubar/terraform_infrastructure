193c29ea3deef1fa76eba27bc84851388f3aa392
METoubar <mtoubar1@gmail.com>
2021-05-25 16:55:50 +0200

    configure IS updates

--- terraform/key.tf
+++ terraform/key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "mykeypair" {
-  key_name   = "myansiblekey"
+  key_name   = "myansikey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "example-instance" {
-  name        = "allow-ssh"
+  name        = "SSH-Conn"
   description = "security group that allows ssh and all egress traffic"
   ingress {
     from_port   = 22
